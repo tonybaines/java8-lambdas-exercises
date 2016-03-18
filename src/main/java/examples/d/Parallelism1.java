117a31c651b129c1c009b1cf56d3a5a3bb17bae2
Tony Baines <tony.baines.1@gmail.com>
2016-03-18 15:54:26 +0000

    More on parallel

--- src/main/java/examples/d/Parallelism1.java
+++ src/main/java/examples/d/Parallelism1.java
@@ -2,6 +2,9 @@ package examples.d;
 
 import support.Sources;
 
+import java.math.BigInteger;
+
+import static com.google.common.math.LongMath.factorial;
 import static support.Util.time;
 
 
@@ -12,20 +15,29 @@ public class Parallelism1 {
     /*
       Which is faster?
      */
+    final int limit = 100000000; // Change the magnitude depending on execution times
     try {
       System.out.println("With parallelism");
       time(() -> Sources.infiniteWordStream()
         .parallel()
-        .limit(100000000)
+        .limit(limit)
+        .map(String::toUpperCase)
         .mapToInt(String::length)
-        .count());
+//        .mapToLong(i -> factorial(i) + 1)
+//        .filter(i -> BigInteger.valueOf(i).isProbablePrime(100))
+        .sum());
 
       System.out.println("Without parallelism");
       time(() -> Sources.infiniteWordStream()
 //        .parallel()
-        .limit(100000000)
+        .limit(limit)
+        .map(String::toUpperCase)
         .mapToInt(String::length)
-        .count());
+//        .mapToLong(i -> factorial(i) + 1)
+//        .filter(i -> BigInteger.valueOf(i).isProbablePrime(100))
+        .sum());
+
+
     } catch (Exception e) {
       e.printStackTrace();
     }
