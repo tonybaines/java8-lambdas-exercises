16ebc154eb1bc74a3744a1d2c1a9213b7d5b26a6
Tony Baines <tony.baines.1@gmail.com>
2016-03-19 11:39:50 +0000

    Refactor step1

--- src/main/java/examples/b/Extension.java
+++ src/main/java/examples/b/Extension.java
@@ -4,6 +4,8 @@ import com.google.common.collect.Lists;
 
 import java.time.LocalDate;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static examples.b.Extension.Books.*;
 
@@ -20,13 +22,18 @@ public class Extension {
    TODO: Refactor this to use streams
    */
   public static Set<String> getReadersOfBooks(Collection<String> readers, Collection<Books> books, LocalDate date) {
-    DataService dataService = new DataService(){ };
+    DataService dataService = new DataService() {
+    };
 
     Set<String> result = new HashSet<>();
     Map<String, Collection<Books>> data = dataService.getBooksReadOn(date);
-    for (Map.Entry<String, Collection<Books>> e : data.entrySet()) {
+    final Stream<Map.Entry<String, Collection<Books>>> entriesWithReadersOfInterest =
+      data.entrySet().stream()
+        .filter(e -> readers.contains(e.getKey()));
+
+    for (Map.Entry<String, Collection<Books>> e : entriesWithReadersOfInterest.collect(Collectors.toSet())) {
       for (Books book : books) {
-        if (e.getValue().contains(book) && readers.contains(e.getKey())) {
+        if (e.getValue().contains(book)) {
           result.add(e.getKey());
         }
       }
@@ -35,25 +42,21 @@ public class Extension {
   }
 
 
-
-
   public enum Books {SpotTheDog, Ulysses, Dune, LordOfTheFlies}
 
-  private interface DataService{
-    default Map<String,Collection<Books>> getBooksReadOn(LocalDate date) {
+  private interface DataService {
+    default Map<String, Collection<Books>> getBooksReadOn(LocalDate date) {
       final Map<String, Collection<Books>> books = new HashMap<>();
       if (date.equals(MARCH_1)) {
         books.put("Tom", Lists.newArrayList(SpotTheDog));
         books.put("Harry", Lists.newArrayList(Dune));
         books.put("Eric", Lists.newArrayList(SpotTheDog, Ulysses));
-      }
-      else if (date.equals(MARCH_2)) {
+      } else if (date.equals(MARCH_2)) {
         books.put("Harry", Lists.newArrayList(Dune));
         books.put("Eric", Lists.newArrayList(Ulysses));
         books.put("Ernie", Lists.newArrayList(SpotTheDog));
         books.put("Bert", Lists.newArrayList(Ulysses, Dune));
-      }
-      else if (date.equals(MARCH_3)) {
+      } else if (date.equals(MARCH_3)) {
         books.put("Ernie", Lists.newArrayList(SpotTheDog, LordOfTheFlies));
         books.put("Bert", Lists.newArrayList(Dune));
       }
